b227f0f98665214a3b5b1881d1d45a3163512ce1
Doston04 <avazovjr@gmail.com>
2022-10-21 16:27:39 +0500

    swiper fixed

--- static/newSwiper.js
+++ static/newSwiper.js
@@ -1,11 +1,25 @@
-var swiper = new Swiper(".newSwiper", {
+var swiper1 = new Swiper(".horizontal_swiper", {
+  navigation: {
+    nextEl: ".next",
+    prevEl: ".prev",
+  },
+  slidesPerView: 1,
+  spaceBetween: 20,
+  speed: 300,
+  allowTouchMove: false
+});
+
+var swiper2 = new Swiper(".vertical_swiper", {
   direction: "vertical",
   slidesPerView: 1,
   spaceBetween: 20,
-  mousewheel: true,
   navigation: {
-    prevEl: "#prevVS",
-    nextEl: "#nextVS",
+    nextEl: ".next",
+    prevEl: ".prev",
   },
-  speed: 1200
+  speed: 300,
+  allowTouchMove: false
 });
+
+swiper1.controller.control = swiper2
+swiper2.controller.control = swiper1
